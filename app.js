dfafe039dee9a4cbe3d384ab770d84117663ecb1
serashioda <seras37@gmail.com>
2016-07-22 15:07:48 -0700

    “

--- app.js
+++ app.js
@@ -64,22 +64,22 @@ IMPORTANT DETAIL: You may not use the arithmetic operators + and * in this funct
 Test this function by hand in the console to get it working, and when you think it is finished, uncomment the call for the testSumAndMultiply() function and see if the test passes.*/
 
 // Write your code here
-// function sumAndMultiply(a,b,c){
-//   var sumOne = sum(a, b)[0];
-//   var sumTwo = sum(sumOne, c)[0];
-//   var message = a + ' and ' + b + ' and ' + c + ' sum to ' + sumTwo + '.';
-//   console.log('4 and 7 and 5 sum to 16. is the expected output.');
-//   console.log(message + ' is the actual output.');
-//
-//   var multiOne = multiply(a, b)[0];
-//   var multiTwo = multiply(multiOne, c)[0];//eslint-disable-line
-//   var message1 = 'The product of ' + a + ' and ' + b + ' and ' + c + ' is ' + multiTwo + '.';
-//   console.log('The product of ' + a + ' and ' + b + ' and ' + c + ' is ' + multiTwo + '.');
-//   console.log(message1 + ' is the actual output.');
-//   return [sumOne, message, message1, multiTwo];
-// }
-// // Here is the test for sumAndMultiply(); uncomment it to run it
-//   testSumAndMultiply(4,7,5);
+function sumAndMultiply(a,b,c){
+  var sumOne = sum(a, b)[0];
+  var sumTwo = sum(sumOne, c)[0];
+  var message = a + ' and ' + b + ' and ' + c + ' sum to ' + sumTwo + '.';
+  console.log('4 and 7 and 5 sum to 16. is the expected output.');
+  console.log(message + ' is the actual output.');
+
+  var multiOne = multiply(a, b)[0];
+  var multiTwo = multiply(multiOne, c)[0];//eslint-disable-line
+  var message1 = 'The product of ' + a + ' and ' + b + ' and ' + c + ' is ' + multiTwo + '.';
+  console.log('The product of ' + a + ' and ' + b + ' and ' + c + ' is ' + multiTwo + '.');
+  console.log(message1 + ' is the actual output.');
+  return [sumOne, message, message1, multiTwo];
+}
+// Here is the test for sumAndMultiply(); uncomment it to run it
+  testSumAndMultiply(4,7,5);
 
 // Once you get the test passing, do an a-c-p cycle and synchronize the code between GitHub and your laptop. Don't forget to create a new branch for your work on the next question!
 
